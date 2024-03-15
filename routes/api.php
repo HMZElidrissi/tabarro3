5cf6fd7c4f6841620e877d62b6c45e7f9de53cba
HMZElidrissi <hamza.ezzharelidrissi1@gmail.com>
2024-03-15 15:44:22 +0000

    feat: Implement participants + organizations management

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,9 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\Organization\CampaignController;
+use App\Http\Controllers\Admin\OrganizationController;
+use App\Http\Controllers\Admin\ParticipantController;
 
 /*
 |--------------------------------------------------------------------------
@@ -23,4 +26,28 @@ Route::controller(AuthController::class)->group(function () {
     Route::post('register', 'register');
     Route::post('logout', 'logout');
     Route::post('refresh', 'refresh');
+});
+
+Route::controller(CampaignController::class)->group(function () {
+    Route::get('campaigns', 'index');
+    Route::post('campaigns', 'store');
+    Route::get('campaigns/{id}', 'show');
+    Route::put('campaigns/{id}', 'update');
+    Route::delete('campaigns/{id}', 'destroy');
+});
+
+Route::controller(OrganizationController::class)->group(function () {
+    Route::get('organizations', 'index');
+    Route::post('organizations', 'store');
+    Route::get('organizations/{id}', 'show');
+    Route::put('organizations/{id}', 'update');
+    Route::delete('organizations/{id}', 'destroy');
+});
+
+Route::controller(ParticipantController::class)->group(function () {
+    Route::get('participants', 'index');
+    Route::post('participants', 'store');
+    Route::get('participants/{id}', 'show');
+    Route::put('participants/{id}', 'update');
+    Route::delete('participants/{id}', 'destroy');
 });
