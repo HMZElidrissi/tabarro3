4641d67dfb1addee04e6f63fcec713410848fd02
HMZElidrissi <hamza.ezzharelidrissi1@gmail.com>
2024-04-17 16:11:24 +0100

    chore: Register the Observer in the App Service Provider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use App\Models\BloodRequest;
+use App\Observers\BloodRequestObserver;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +21,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        BloodRequest::observe(BloodRequestObserver::class);
     }
 }
