b64ebf3d9b26228ce2bbf878539a6ea3e94edb52
HMZElidrissi <hamza.ezzharelidrissi1@gmail.com>
2024-04-17 18:56:58 +0100

    refactor: redefine factories

--- database/factories/CampaignFactory.php
+++ database/factories/CampaignFactory.php
@@ -2,10 +2,12 @@
 
 namespace Database\Factories;
 
+use App\Models\Campaign;
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Campaign>
+ * @extends Factory<Campaign>
  */
 class CampaignFactory extends Factory
 {
@@ -20,8 +22,8 @@ class CampaignFactory extends Factory
             'name' => fake()->sentence(6, true),
             'description' => fake()->paragraph(3, true),
             'location' => fake()->city(),
-            'start_time' => fake()->dateTime(),
-            'end_time' => fake()->dateTime(),
+            'start_time' => Carbon::create(2024, 4, 25, 0, 0, 0)->addDays($this->faker->numberBetween(1, 365)),
+            'end_time' => Carbon::create(2024, 4, 26, 0, 0, 0)->addDays($this->faker->numberBetween(20, 30)),
             'organization_id' => $this->faker->numberBetween(1, 10),
         ];
     }
--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -3,7 +3,6 @@
 namespace Database\Seeders;
 
 use App\Models\User;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class UserSeeder extends Seeder
@@ -13,14 +12,35 @@ class UserSeeder extends Seeder
      */
     public function run(): void
     {
-        User::factory()
-            ->count(10)
-            ->create();
+        User::create([
+            'name' => 'Meryem El Herrachy',
+            'email' => 'hamza.ezzharelidrissi1@gmail.com',
+            'password' => bcrypt('password'),
+            'city' => 'Casablanca',
+            'phone' => '0612378678',
+            'blood_group' => 'AB+',
+            'role' => 2,
+        ]);
+        User::create([
+            'name' => 'Khalil Hamdoune',
+            'email' => 'khalil@khalil.com',
+            'password' => bcrypt('password'),
+            'city' => 'Fes',
+            'phone' => '0670345678',
+            'blood_group' => 'O+',
+            'role' => 3,
+        ]);
         User::create([
             'name' => 'Amine Ismaili',
             'email' => 'amine@amine.com',
-            'password' => bcrypt('amine'),
+            'password' => bcrypt('password'),
+            'city' => 'Casablanca',
+            'phone' => '0612345678',
+            'blood_group' => 'A+',
             'role' => 1,
         ]);
+        User::factory()
+            ->count(7)
+            ->create();
     }
 }
