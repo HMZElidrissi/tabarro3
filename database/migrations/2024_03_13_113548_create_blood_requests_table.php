3c29421563b2ec8f117b836d0ca409e9d07092a7
Hamza El Idrissi <hamza.ezzharelidrissi1@gmail.com>
2024-10-17 14:11:45 +0100

    ci: clean up

--- database/migrations/2024_03_13_113548_create_blood_requests_table.php
+++ database/migrations/2024_03_13_113548_create_blood_requests_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('blood_requests', function (Blueprint $table) {
             $table->id();
             $table->string('description');
-            $table->enum('blood_group', ['A+', 'A-', 'B+', 'B-', 'AB+', 'AB-', 'O+', 'O-']);
+            $table->enum('blood_group', ['A+', 'A-', 'B+', 'B-', 'AB+', 'AB-', 'O+', 'O-'])->nullable();
             $table->string('city');
             $table->enum('status', ['open', 'closed'])->default('open');
             $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
