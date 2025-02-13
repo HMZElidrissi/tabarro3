5ba8987ef1c98b3a4f4208b24258b897a713ed97
hmzelidrissi <hamza.ezzharelidrissi1@gmail.com>
2025-02-13 22:52:01 +0100

    enable partners

--- components/home/partners.tsx
+++ components/home/partners.tsx
@@ -2,6 +2,51 @@ import Marquee from 'react-fast-marquee';
 import Image from 'next/image';
 
 export default function Partners({ dict }: { dict: any }) {
+    const partners = [
+        {
+            name: 'Ibn Sina',
+            logo: '/partners/ibnsina-green.png',
+            width: 400,
+            height: 250,
+            className: 'w-24 h-auto object-contain',
+        },
+        {
+            name: 'Rotaract Tanger Doyen',
+            logo: '/partners/rotaract-tanger-doyen.png',
+            width: 600,
+            height: 200,
+            className: 'w-40 h-auto object-contain',
+        },
+        {
+            name: 'dir iddik',
+            logo: '/partners/dir-iddik-518x137.png',
+            width: 518,
+            height: 137,
+            className: 'w-40 h-auto object-contain',
+        },
+        {
+            name: 'Rotaract ENSAF',
+            logo: '/partners/rotaract-ensaf.png',
+            width: 600,
+            height: 200,
+            className: 'w-40 h-auto object-contain',
+        },
+        {
+            name: 'SPARK EST ESSAOUIRA',
+            logo: '/partners/spark.png',
+            width: 500,
+            height: 500,
+            className: 'w-24 h-auto object-contain',
+        },
+        {
+            name: 'Rotaract Ryad Ennakhil',
+            logo: '/partners/rotaract-ryad-ennakhil.png',
+            width: 600,
+            height: 200,
+            className: 'w-40 h-auto object-contain',
+        },
+    ];
+
     return (
         <section className="py-8">
             <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 text-center">
@@ -11,13 +56,21 @@ export default function Partners({ dict }: { dict: any }) {
 
                 <div className="py-8">
                     <Marquee gradient={false} speed={50}>
-                        <Image
-                            src="/partners/ibnsina-green.png"
-                            alt="Ibn Sina's logo"
-                            width={100}
-                            height={100}
-                            className="w-24 mr-24 "
-                        />
+                        <div className="flex items-center gap-8">
+                            {partners.map((partner, index) => (
+                                <div
+                                    key={index}
+                                    className="flex items-center justify-center h-24 overflow-hidden">
+                                    <Image
+                                        src={partner.logo}
+                                        alt={partner.name}
+                                        width={partner.width}
+                                        height={partner.height}
+                                        className={partner.className}
+                                    />
+                                </div>
+                            ))}
+                        </div>
                     </Marquee>
                 </div>
             </div>
